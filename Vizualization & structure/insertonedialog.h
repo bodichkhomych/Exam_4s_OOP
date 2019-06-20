ba118683a8f113d5a88915d93db55614fadab868
bodichkhomych <inesh69inesh@gmail.com>
2019-06-20 10:32:10 +0300

    Started working on Visitor pattern

---
+++ Vizualization & structure/insertonedialog.h
@@ -0,0 +1,24 @@
+#ifndef INSERTONEDIALOG_H
+#define INSERTONEDIALOG_H
+
+#include <QDialog>
+
+namespace Ui {
+class InsertOneDialog;
+}
+
+class CoreFacade;
+class InsertOneDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit InsertOneDialog(QWidget *parent = nullptr);
+    ~InsertOneDialog();
+private slots:
+    void insertBtn_clicked();
+private:
+    Ui::InsertOneDialog *ui;
+};
+
+#endif // INSERTONEDIALOG_H
